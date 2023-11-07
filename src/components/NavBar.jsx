ec1363a145454823a5ccdf108cda11400de8f70b
pratiksahu <pratik.sahu1421996@gmail.com>
2023-11-06 20:02:36 -0500

    changed logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import {FaBars, FaTimes} from "react-icons/fa";
 import {Link} from "react-scroll";
+import logoicon from '../assets/logoicon.png'
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
@@ -40,9 +41,14 @@ const NavBar = () => {
 
   return (
       <div className="flex flex-wrap justify-between items-center w-full h-20 px-4 text-white bg-gray-800 bg-opacity-50 fixed">
-          <div>
-              <h1 className="text-3xl sm:text-4xl md:text-5xl font-signature ml-2">Pratik Sahu</h1>
-          </div>
+          <div className="flex items-center">
+              <img
+                src={logoicon} // Replace with the URL of your image
+                alt="Pratik Sahu"
+                className="w-10 h-10 rounded-full mr-2" // Use the 'rounded-full' class to make it circular
+              />
+              <h1 className="text-3xl sm:text-3xl font-bold md:text-3xl">Pratik Sahu</h1>
+            </div>
 
           <ul className="hidden md:flex">
               {links.map(({ id, link, name }) => (
